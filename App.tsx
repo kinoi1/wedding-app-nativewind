a3479de9b43465bc86cfb971761e38f42b852d54
kinoi1 <agilsupriyanto14@gmail.com>
2025-10-25 13:47:48 +0700

    tes

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ export default function App() {
   return (
     <View className='bg-blue-500'>
       <Text className='font-extrabold p-2'>
-        hello ini dari nativewind v2
+        hello ini dari nativewind v2 new
       </Text>
     </View>
   );
